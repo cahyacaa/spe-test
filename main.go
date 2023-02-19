1d3317f21fc1a78617826112f7cdca2744ce242b
cahyacaa <cahyanug3@gmail.com>
2023-02-19 15:20:08 +0700

    add blue ocean reverse

--- main.go
+++ main.go
@@ -62,8 +62,39 @@ func NeedleHaystack(arr []string, query string) int {
 	return -1
 }
 
+func BlueOceanReverse(arr1, arr2 []int) []int {
+	result := make([]int, 0, 0)
+	numMap := make(map[int]bool)
+
+	if len(arr1) < len(arr2) {
+		for _, val := range arr2 {
+			numMap[val] = true
+		}
+	} else {
+		for _, val := range arr2 {
+			numMap[val] = true
+		}
+	}
+
+	if len(arr1) < len(arr2) {
+		for _, val := range arr2 {
+			if _, ok := numMap[val]; !ok {
+				result = append(result, val)
+			}
+		}
+	} else {
+		for _, val := range arr1 {
+			if _, ok := numMap[val]; !ok {
+				result = append(result, val)
+			}
+		}
+	}
+	return result
+}
+
 func main() {
 	fmt.Println(NarsisticNumber(153))
 	fmt.Println(PartyOutliers([]int{160, 3, 1719, 19, 11, 13, -21}))
 	fmt.Println(NeedleHaystack([]string{"red", "blue", "yellow"}, "blue"))
+	fmt.Println(BlueOceanReverse([]int{1, 5, 5, 5, 5, 5, 3}, []int{5}))
 }
